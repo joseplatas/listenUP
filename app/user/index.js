876d00146f905e58b4610d379deae18b50f82bc2
Jennifer Tsan <jenn.tsan@gmail.com>
2017-09-23 17:23:29 -0700

    Update index.js

--- app/user/index.js
+++ app/user/index.js
@@ -4,5 +4,6 @@ const fns = require('./fns.js')
 const templates = require('./templates.js')
 require('./styles.css')
 
+//test code
 data.getUser()
     .then(data => window.alert("Received message from user api: " + data))
