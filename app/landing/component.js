3f44fa1f037b00d9f7df1d6c91f14759144274c4
Jennifer Tsan <jenn.tsan@gmail.com>
2017-09-26 17:24:18 -0700

    started landing page

--- app/landing/component.js
+++ app/landing/component.js
@@ -10,12 +10,38 @@ import {
   } from 'react-router-dom'
 
 export const LandingPage = () =>
-    <div>
-        <h1>listenUP</h1>
+    <div id='body'>
         <div id='header'>
             <ul>
                 <li><Link to='/login'>Log In</Link></li>
-                <li><Link to='/transcription'>First Exercise</Link></li>
             </ul>
+            <h1>listenUP</h1>
         </div>
+
+        <div className='panel_slateBG'>
+
+                <h2>Practice your listening comprehension by listening to how native speakers <em>actually</em> talk.</h2>
+
+                <div id='getStartedButton'>
+                    <Link to='/signup'>Get Started</Link> 
+                </div>
+
+            </div>
+
+        <div className='panel_whiteBG'>
+
+            <h4>So, you still can't understand anything that native speakers are saying. Welcome to the "Intermediate Hell" of language learning.</h4>
+
+            {/*--- PARTS OF "ABOUT" FOLLOW HERE---*/}
+
+            </div>
+        
+        <div className='panel_mintBG'>
+
+            <h3>How it works</h3>
+
+            {/*--- PARTS OF "HOW IT WORKS" FOLLOW HERE---*/}
+
+            </div>
+
     </div>
