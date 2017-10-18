32a983fe3f1c1151d7eeabc37bc745daf8c81a81
Jennifer Tsan <jenn.tsan@gmail.com>
2017-10-18 15:28:27 -0700

    updated styling of home

--- app/home/component.js
+++ app/home/component.js
@@ -21,99 +21,128 @@ function classes(...classNames) {
 
 export const Home=() =>
 
-    <div className={classes('wrapper')}>
-        <section className={classes('intro')}>  
-            <div className={classes('content')}>
+<div className='homePage flex_container'>
 
-                <h1 className={classes('whiteHeader')}>Practice your listening comprehension
-                    by listening to how natives <strong>actually</strong> talk.
-                </h1>
-
-                <div className={classes('centerButton', 'signupButton')}>
-                    <Link to='/Transcription'>Get Started</Link>
-                </div>
-
-            </div>
-        </section>
-
-        <section className={styles.about}>
-            <div className={styles.content}>
-
-                <h2>So, you still can’t understand anything that native speakers are saying.
-                    Welcome to the <em>«Intermediate Hell»</em> of language learning.</h2>
-                <h1 className={classes('blueHeader')}>It's time to listenUP.</h1>
-
-                <div className={classes('column-container')}>
-                    
-                    <div className={classes('col')}>
-                        <h4>real speakers.</h4>
-                        <p>Listen to audio clips of native
-                            speakers in your target language
-                            as they answer questions or
-                            discuss everyday topics.</p>
-                    </div>
-
-                    <div className={classes('col')}>
-                        <h4>real talk.</h4>
-                        <p>Ditch the scripted conversations
-                            for beginners and get a feel for how
-                            natives truly express themselves,
-                            like, um... filler words and all!</p>
-                    </div>
-
-                    <div className={classes('col')}>        
-                        <h4>real effective.</h4>
-                        <p>All 2 of our test subjects have
-                            agreed that listenUp is helpful.
-                            That’s, like, half of our entire
-                            development team!</p>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-
-        <section className={classes('howitworks')}>
-            <div className={classes('content')}>
-                <h3>how it works</h3>
-
-                <div className={classes('column-container')}>
-                        
-                        <div className={classes('col')}>
-                            <img src ={ss_Listen}/>
-                            <h4>listen.</h4>
-                            <p>We'll play short clips of native speakers 
-                                having natural conversations, answering 
-                                interview questions, or doing skits.
-                            </p>
-                        </div>
-        
-                        <div className={classes('col')}>
-                            <img src ={ss_Answer}/>
-                                                
-                            <h4>answer.</h4>
-                            <p>We offer two exercises: Transcription and Quiz. 
-                                You'll either transcribe the given audio clip or 
-                                take a mini-quiz on the content presented.
-                            </p>                            
-                        </div>
-        
-                        <div className={classes('col')}>
-                            <img src ={ss_Learn}/>                            
-                            <h4>learn.</h4>
-                            <p>Get scored based on your answer. Review exercises 
-                                and improve over time with repetition. You'll be 
-                                listening like a pro in no time!
-                            </p>                                                        
-                        </div>
-        
-                    </div>
-
-            </div>
-        </section>
+    <section className='welcome flex_container full_height'>
     
-    <footer>
-        <a href="#" className={classes('whiteLink')}>Credits</a>
-    </footer>
-
+    <h3 className='welcome_header centered_text'>
+        Train your listening comprehension skills
+        by listening to how native speakers 
+        <em className='welcome_em'> actually</em> talk.
+    </h3>
+    
+    <div className='welcome_getStartedButton centered_text'>
+        <a className='actionButton' href='#'>
+        Get Started
+        </a>
     </div>
+    
+    </section>
+
+    <section className='about flex_container full_height'>
+    
+    <h3 className='about_header centered_text'>
+        So, you still can’t understand anything that
+        native speakers are saying. Welcome to the
+        «Intermediate Hell» of language learning.
+    </h3>
+    <h1 className='big_blue_header centered_text'>
+        It's time to listenUP.
+    </h1>
+    
+    <section className='details_container flex_container'>
+        
+        <div className='detail flex_container'>
+        <h4 className='detail_header'>
+            real people.
+        </h4>
+        <p className='detail_body'>
+            Listen to audio clips of native speakers in 
+            your target language as they answer questions 
+            or discuss everyday topics.
+        </p>
+        </div>
+        
+        <div className='detail flex_container'>
+        <h4 className='detail_header'>
+            real talk.
+        </h4>
+        <p className='detail_body'>
+            Ditch the scripted conversations 
+            for beginners & get a feel for how 
+            natives truly express themselves, 
+            ...um...like... filler words and all!
+        </p>
+        </div>
+        
+        <div className='detail flex_container'>
+        <h4 className='detail_header'>
+            real effective.
+        </h4>
+        <p className='detail_body'>
+            All 2 of our test subjects have agreed that 
+            listenUp is helpful. That’s, like, half of 
+            our entire development team!
+        </p>
+        </div>
+        
+    </section>
+    </section>
+
+    <section className='howitworks flex_container full_height'>
+    
+    <h3 className='howitworks_header centered_text'>
+        how it works
+    </h3>
+    
+    <section className='details_container flex_container'>
+        <div className='detail flex_container'>
+        <div className='detail_thumbnail'>
+        </div>
+        <h4 className='detail_header'>
+            listen.
+        </h4>
+        <p className='detail_body'>
+            We'll play a short audio or video clip 
+            of a native speaker. All of our clips have 
+            been specially chosen to expose you to how 
+            people speak in a natural environment, 
+            without slowing themselves down for learners.
+        </p>
+        </div>
+        
+        <div className='detail flex_container'>
+        <div className='detail_thumbnail'>
+        </div>
+        <h4 className='detail_header'>
+            answer
+        </h4>
+        <p className='detail_body'>
+            We offer two types of exercises: Transcription 
+            and Quiz. In Transcription, all you have to do 
+            is write down what you hear, excluding non-word 
+            fillers like "umm." In Quiz Mode, you'll answer 
+            a question that tests your comprehension.
+        </p>
+        </div>
+        
+        <div className='detail flex_container'>
+        <div className='detail_thumbnail'>
+        </div>
+        <h4 className='detail_header'>
+            learn.
+        </h4>
+        <p className='detail_body'>
+            In either learning mode, you'll earn points based 
+            on your answer. We'll also cycle back through the 
+            difficult exercises for you, so you have plenty 
+            of opportunities to improve. Over time, you'll 
+            become more comfortable with listening to how 
+            native speakers really talk!
+        </p>
+        </div>
+    </section>
+    
+    </section>
+
+</div>
