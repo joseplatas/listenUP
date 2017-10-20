3a1fcb4bf54a0286be9a51f798e1150befada717
Krystina Kuba <krystinakuba777@gmail.com>
2017-10-20 10:03:00 -0700

    code snippet for user input and repetition

---
+++ api/exercises/fnsRepetitionCodeSnippet.js
@@ -0,0 +1,19 @@
+constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+    this.handleCheck = this.handleCheck.bind(this)
+    this.handleRepeat = this.handleRepeat.bind(this)
+}
+
+handleRepeat()
+{
+    this.handleCheck(val) == false ?
+    this.setState({
+
+    });
+    alert('No repetition required:')
+}
+
+handleCheck()
