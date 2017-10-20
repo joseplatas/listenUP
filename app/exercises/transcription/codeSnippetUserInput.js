71cdba41c94ee16fe1eedecf2f2d6d3fb96d1d5d
Krystina Kuba <krystinakuba777@gmail.com>
2017-10-20 16:20:48 -0700

    updated code snippet for user input

--- app/exercises/transcription/codeSnippetUserInput.js
+++ app/exercises/transcription/codeSnippetUserInput.js
@@ -1,18 +1,44 @@
 constructor(props){
-    super(props);
-    this.state = {
-      answer: {answer: ''},
+ super(props)
+ this.state = {
+     counter: 0,
+     questionId: 1,
+     question: '',
+     answer: '',
+     answerCount: {
+         someChoice: 0,
+     }
+ }
+}
+
+handleAnswerSelected(event){
+    this.setAnswer(event.currentTarget.value)
+    if(this.state.question < exercise.length)
+    {
+        this.setNextQuestion()
+    }
+    else{
+
     }
 }
 
-handleAnswer(event) {
+setUserAnswer(answer){
+    const updatedAnswerCount = update(this.state.answerCount, {
+        [answer]: {$apply: (currentValue) => currentValue+1}
+    });
     this.setState({
-        answer: event.target.value
+        answerCount: updatedAnswerCount,
+        answer: answer
     })
 }
 
-handleSubmit(event){
-    var answer = this.state.answer
-    alert('User Answer:'+answer)
-    event.preventDefault();
+setNextQuestion(){
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+        counter: counter,
+        questionId: questionId,
+        question: quizQuestion[counter].answer,
+        answer: ''
+    })
 }
