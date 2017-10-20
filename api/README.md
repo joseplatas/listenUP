7744af6a3917cac3bc8e390cc47cc6b2fc825b5f
jplatasCuracao <=>
2017-10-20 15:49:31 -0700

    Created skeleton module for registration. It still needs more validation but it will create the user already. Login Module and sessions are still being created.

---
+++ api/README.md
@@ -0,0 +1,7 @@
+# SOME DESCRIPTION TO TEST form
+I change the port of the api server to 8080 to avoid problems in my machine
+
+## test urls:
+
+- http://localhost:8080/api/user/login
+- http://localhost:8080/api/user/register
--- api/user/index.js
+++ api/user/index.js
@@ -1,10 +1,72 @@
 const fns = require('./fns.js')
 const data = require('./data.js')
+//initialize
+var express = require('express');
+var path = require('path');
+var userRouter = express.Router();
+var User = require('./models/users');
 
-module.exports = {
-    configure: function (app) {
-        app.get('/api/user', function(req, res) {
-            res.send("Hello, from users system!")
-        })
+userRouter.get('/', function(req, res, next) {
+    res.send("user router");
+});
+
+userRouter.get('/login', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'/test/loginForm.html'));
+});
+
+userRouter.post('/loginPost', function(req, res, next){
+    //PENDING: pending logic to login and create session
+    res.send('login post')
+
+});
+//register test form
+userRouter.get('/register', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'/test/registerForm.html'));
+});
+//register form handler
+userRouter.post('/registerPost', function(req, res, next){
+  //TO DO IN POST
+  //check if all parameters are pass
+  if(req.body.email &&
+    req.body.fname &&
+    req.body.lname &&
+    req.body.password &&
+    req.body.confirmPassword
+  ){
+    console.log("All Parameters pass");
+  }else{
+    console.log("Please enter all parameters");
+  }
+  //check if passwords are matching
+  if(req.body.password !== req.body.confirmPassword){
+    //PENDING: need code to valid strength of password
+    console.log("Passwords are not matching");
+  }
+  //assign data to userData object
+  var userData = {
+    email: req.body.email,
+    fname: req.body.fname,
+    lname: req.body.lname,
+    password: req.body.password,
+    confirmPassword: req.body.password
+  };
+  //call function and insert to collection
+
+  User.create(userData,function(error, user){
+    if(error){
+      console.log("Error Creating user");
+      console.log(error);
+      res.send(error);
+    }else{
+      //PENDING: log the user in and send them to dashboard
+      console.log(user);
+      res.send('USER CREATED: '+user.email);
     }
-}
+  });
+
+});
+
+
+
+//export routes to /api/index.js
+module.exports = userRouter;
