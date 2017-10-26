5faa5dc9064d01bd301f7551873607988be9d142
Jose Platas <jplatas@outlook.com>
2017-10-26 14:06:59 -0700

    Added login in basic functionality. Needs more work on session values.

--- api/user/index.js
+++ api/user/index.js
@@ -14,9 +14,43 @@ userRouter.get('/login', function(req, res, next) {
     res.sendFile(path.join(__dirname+'/test/loginForm.html'));
 });
 
+//login handler
 userRouter.post('/loginPost', function(req, res, next){
-    //PENDING: pending logic to login and create session
-    res.send('login post')
+    res.setHeader('Content-Type', 'application/json');
+    var response = {};
+    //console.log("Data send from react");
+    //console.log(res.body);
+    //check if all parameters are pass
+    if(req.body.email &&
+      req.body.password
+    ){
+      //ALL VALUES WERE PASS
+    }else{
+      response.err = -1;
+      response.message = "Missing values";
+      res.send(response);
+    }
+    //check if the user exist
+    User.authenticate(req.body.email, req.body.password, function(error, user){
+      //console.log(error);
+      //console.log(user);
+      if(error || !user){
+        response.err = -1;
+        response.message = "User found";
+        response.error = error;
+        res.send(response);
+      }else{
+        //add session variable
+        req.session.userId = user._id;
+        response._user_id = user._id;
+        response.err = 0;
+        response.message = "User found";
+        res.send(response)
+      }
+    });
+
+
+
 
 });
 //register test form
